dd1d4ae4667b18b7736304a994251a37d0e9c84f
Landry Karege <Landry.Karege@avalon.net>
2018-07-25 12:27:53 +0100

    Update

--- src/client/app.js
+++ src/client/app.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Styles from './app.scss';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
       return (
          <h1>Hello from React!</h1>
       );
   }
 }
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
