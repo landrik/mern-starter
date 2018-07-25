cfffd4539acb0cd0f136a9493dd6ec29b81ead0a
Landry Karege <Landry.Karege@avalon.net>
2018-07-25 11:10:54 +0100

    init commit

--- src/client/app.js
+++ src/client/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Styles from './app.scss';
 
 class App extends React.Component {
-
   render() {
       return (
          <h1>Hello from React!</h1>
