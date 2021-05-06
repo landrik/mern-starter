015b83e4af6867ed7df7e10667670864dd8b6b6a
Landry Karege <you@example.com>
2021-05-06 10:45:27 +0100

    updated client folder

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import {
+  Container,
+  FormGroup,
+  Form,
+  Input,
+  Button,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col
+} from "reactstrap";
 
 const Login = () => {
   return (
-    <div>
-      login page
-    </div>
+    <Container>
+      <Row>
+        <Col sm={{ size: 4, offset: 4 }} className="pt-5">
+          <p className="h5">back to home</p>
+          <p className="h2">Login below</p>
+          <p className="h6">Don't have an account? Register</p>
+          <Form>
+              <FormGroup>
+                <Input
+                  className="form-control-alternative"
+                  id="email"
+                  placeholder="name@example.com"
+                  type="email"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Input
+                  className="form-control-alternative"
+                  id="password"
+                  placeholder="password"
+                  type="password"
+                />
+              </FormGroup>
+              <Button color="primary" type="button" size="lg"> Login </Button>
+          </Form>
+        </Col>
+      </Row>
+      
+    </Container>
+    
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// import "assets/css/nucleo-svg.css";
+// import "assets/css/nucleo-icons.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "assets/scss/argon-design-system.scss?v1.0.0";
+
+//import "assets/vendor/nucleo/css/nucleo.css";
+//import "assets/vendor/font-awesome/css/font-awesome.min.css";
+//import "assets/scss/argon-design-system-react.scss";
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
